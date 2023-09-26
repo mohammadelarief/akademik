2da6b15843d4a8ec2c407955be10f326c72c1ad3
mohammadelarief <mohammadelarief23@gmail.com>
2023-09-26 11:33:30 +0700

    Function Migration

--- application/views/migration_list.php
+++ application/views/migration_list.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-8">
         <!-- Horizontal Form -->
         <div class="box box-info">
             <div class="box-header with-border">
@@ -10,7 +10,7 @@
             <form class="form-horizontal" action="<?php echo base_url('migration/run_migration'); ?>" method="post">
                 <div class="box-body">
                     <div class="form-group">
-                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                        <label for="inputEmail3" class="col-sm-2 control-label">File Migration</label>
 
                         <div class="col-sm-10">
                             <select class="form-control" name="migration_version">
@@ -24,7 +24,7 @@
                 <!-- /.box-body -->
                 <div class="box-footer">
                     <!-- <button type="submit" class="btn btn-default">Cancel</button> -->
-                    <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                    <button type="submit" class="btn btn-info pull-right">Running Migration</button>
                 </div>
                 <!-- /.box-footer -->
             </form>
