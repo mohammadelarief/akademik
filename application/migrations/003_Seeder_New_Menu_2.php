ed5515b57e754cee5e643434f4a306675a22586a
mohammadelarief <mohammadelarief23@gmail.com>
2023-09-27 12:00:54 +0700

    Seeder Menu Unit

---
+++ application/migrations/003_Seeder_New_Menu_2.php
@@ -0,0 +1,49 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Seeder_New_Menu_2 extends CI_Migration
+{
+
+    public function up()
+    {
+        // Data yang ingin Anda tambahkan ke tabel
+        $data = array(
+            'sort' => 5,
+            'level' => 2,
+            'parent_id' => 99,
+            'icon' => 'fas fa-school',
+            'label' => 'Unit',
+            'link' => 'unit',
+            'id' => '#',
+            'id_menu_type' => 1
+        );
+
+        // Menambahkan data ke tabel 'nama_tabel'
+        $this->db->insert('menu', $data);
+        $primary_key = $this->db->insert_id();
+
+        $datas = array(
+            'id_groups' => 1,
+            'id_menu' => $primary_key,
+        );
+        $this->db->insert('groups_menu', $datas);
+        // Jika Anda ingin menambahkan beberapa data sekaligus, Anda dapat menggunakan insert_batch
+        // $data_batch = array(
+        //     array('nama' => 'Nama1', 'email' => 'email1@example.com', 'telepon' => '123-456-7891'),
+        //     array('nama' => 'Nama2', 'email' => 'email2@example.com', 'telepon' => '123-456-7892'),
+        //     // ... tambahkan data lainnya
+        // );
+        // $this->db->insert_batch('nama_tabel', $data_batch);
+    }
+
+    public function down()
+    {
+        // Jika Anda ingin menghapus data yang telah ditambahkan saat migrasi dirollback,
+        // Anda dapat menggunakan $this->db->delete()
+        // Contoh:
+        // $this->db->where('nama', 'John Doe');
+        // $this->db->delete('nama_tabel');
+
+        // Atau Anda dapat membuat skrip rollback yang sesuai dengan kebutuhan Anda.
+    }
+}
